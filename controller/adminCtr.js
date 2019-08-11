8c891712ce7b7379f6135f5db40a35705145d07d
tony <tony860725@gmail.com>
2019-08-11 11:03:08 +0800

    product add, remove, edit

--- controller/adminCtr.js
+++ controller/adminCtr.js
@@ -48,9 +48,79 @@ exports.createProduct = async (req, res, next) => {
 
 exports.updateProduct = async (req, res, next) => {
     const productId = req.params.productId;
+    const userId = req.userId;
+    const name = req.body.name;
+    const price = req.body.price;
+    const imageUrl = req.file.path;
+    const description = req.body.description;
+    let oldimageUrl
+    try {
+        const product = await Product.findOne({
+            _id: productId
+        });
+        if (!product) {
+            const error = new Error('Can not found this product.');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (product.creator.toString() !== userId.toString()) {
+            const error = new Error('Not authorization.');
+            error.statusCode = 401;
+            throw error;
+        }
+        product.name = name;
+        product.price = price;
+        oldimageUrl = product.imageUrl;
+        product.description = description;
+        if (oldimageUrl !== imageUrl) {
+            product.imageUrl = imageUrl;
+            fileHelp.deleteFile(oldimageUrl);
+        }
+        const result = await product.save();
+        res.status(200).json({
+            message: 'Updated successfully',
+            data: result
+        });
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
 
 exports.delectProduct = async (req, res, next) => {
     const productId = req.params.productId;
+    const userId = req.userId;
+    try {
+        const product = await Product.findById(productId);
+        if (!product) {
+            const error = new Error('Cant found this product.');
+            error.statusCode = 404;
+            throw error;
+        }
+        if (product.creator.toString() !== userId.toString()) {
+            const error = new Error('Not authorization.');
+            error.statusCode = 401;
+            throw error;
+        }
+        fileHelp.deleteFile(product.imageUrl);
+        await Product.deleteOne({
+            _id: productId
+        });
+        const user = await User.findById(userId);
+        user.products.pull(productId);
+        await user.save();
 
+        res.status(200).json({
+            message: 'Delete successfully',
+        });
+
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
